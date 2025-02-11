c3c5df88529ec75c99c57bcbde48164e88a7a670
Victor Bertoli <bertoli.victor91@gmai.com>
2025-02-11 11:26:41 -0300

    Iniciar projeto

---
+++ js/app.js
@@ -0,0 +1,7 @@
+function alterarStatus(id) {
+    let gameClicado = document.getElementById(`game-${id}`);
+    let imagemBotao = gameClicado.querySelector('.dashboard__item__img');
+    let Botao = gameClicado.querySelector('.dashboard__item__button');
+    let nomejogo = gameClicado.querySelector('.dashboard__item__name');
+    
+}    
