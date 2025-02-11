95fb69bee4581914c4802b6e7d94364e4dfe15a3
Victor Bertoli <bertoli.victor91@gmai.com>
2025-02-11 13:06:25 -0300

    Finalizar projeto

--- js/app.js
+++ js/app.js
@@ -2,6 +2,14 @@ function alterarStatus(id) {
     let gameClicado = document.getElementById(`game-${id}`);
     let imagemBotao = gameClicado.querySelector('.dashboard__item__img');
     let Botao = gameClicado.querySelector('.dashboard__item__button');
-    let nomejogo = gameClicado.querySelector('.dashboard__item__name');
-    
+
+    if (imagemBotao.classList.contains('dashboard__item__img--rented')){
+        imagemBotao.classList.remove('dashboard__item__img--rented');
+        Botao.classList.remove('dashboard__item__button--return');
+        Botao.textContent = 'Alugar';
+    }   else {
+        imagemBotao.classList.add('dashboard__item__img--rented');
+        Botao.classList.add('dashboard__item__button--return');
+        Botao.textContent = 'Devolver';
+    }
 }    
