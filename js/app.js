fda44eb2df69e85cb027cd2b5770374ea3761b3b
Victor Bertoli <bertoli.victor91@gmai.com>
2025-02-11 14:00:55 -0300

    adicionar confirmação de devolução e contador de alugados

--- js/app.js
+++ js/app.js
@@ -1,3 +1,9 @@
+let jogosAlugados = 0;
+
+function contadorLocacoes() {
+    console.log(`Total de jogos alugados: ${jogosAlugados}`);
+}
+
 function alterarStatus(id) {
     let gameClicado = document.getElementById(`game-${id}`);
     let imagemBotao = gameClicado.querySelector('.dashboard__item__img');
@@ -7,9 +13,18 @@ function alterarStatus(id) {
         imagemBotao.classList.remove('dashboard__item__img--rented');
         Botao.classList.remove('dashboard__item__button--return');
         Botao.textContent = 'Alugar';
+        alert('Por favor, confirme se o jogo a ser devolvido está correto!');
+        jogosAlugados --;
     }   else {
         imagemBotao.classList.add('dashboard__item__img--rented');
         Botao.classList.add('dashboard__item__button--return');
         Botao.textContent = 'Devolver';
+        jogosAlugados ++;
     }
-}    
+    contadorLocacoes();
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    jogosAlugados = document.querySelectorAll('.dashboard__item__img--rented').length;
+    contadorLocacoes();
+});
